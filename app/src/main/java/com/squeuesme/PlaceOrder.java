4b9cc672800308fa98f9a21766f69f3ddf7f4e44
Castro-711 <ecassells711@gmail.com>
2018-02-04 22:06:42 +0000

    Added a 'Remove Last Drink' button to the PlaceOrder activity and it is working as desired

--- app/src/main/java/com/squeuesme/PlaceOrder.java
+++ app/src/main/java/com/squeuesme/PlaceOrder.java
@@ -29,6 +29,7 @@ public class PlaceOrder extends AppCompatActivity {
 
     private Button placeOrder;
     private Button addToOrder;
+    private Button removeLastDrink;
 
     private EditText drinkName;
     private String drinkNameString;
@@ -46,21 +47,22 @@ public class PlaceOrder extends AppCompatActivity {
 
         placeOrder = findViewById(R.id.btnPlaceOrder);
         addToOrder = findViewById(R.id.btnAddToOrder);
+        removeLastDrink = findViewById(R.id.btnRemoveLastDrink);
 
         drinkName = findViewById(R.id.edtDrinkName);
         drinkQuantity = findViewById(R.id.edtDrinkQuantity);
 
-        ordersBoard = new OrdersBoard(); // edit orders board to get date and unique id
+        // edit orders board to get date and unique id
+        ordersBoard = new OrdersBoard();
         currentOrder = new Order();
 
+        // dyamically adding data to listView
         listView = findViewById(R.id.lvCurrentOrder);
         listItems = new ArrayList<>();
-
         adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_expandable_list_item_1,
                 listItems);
-
         listView.setAdapter(adapter);
 
         venue = new Venue("Boomers", "Woodford", ordersBoard);
@@ -74,6 +76,8 @@ public class PlaceOrder extends AppCompatActivity {
 
                 customer.placeOrder(currentOrder);
 
+                listView.setBackgroundColor(345);
+
                 // need to reset currentOrders values
                 currentOrder = new Order();
             }
@@ -86,20 +90,37 @@ public class PlaceOrder extends AppCompatActivity {
                 drinkNameString = String.valueOf(drinkName.getText());
                 getQuantity();
 
+                // create and add drink to order
                 Drink current = new Drink(drinkNameString, drinkQuantityInt);
-
                 currentOrder.addDrinkToOrder(current);
-
+                // add it to listItems, so reflected on listView
                 listItems.add(current.toString());
                 adapter.notifyDataSetChanged();
 
+                // clean up after drink entered
                 wipeEditTextValues();
                 drinkName.requestFocus();
+            }
+        });
 
-                for(String i: listItems)
-                    Log.i("listItem ", i + "");
+        removeLastDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int listSize = listItems.size();
 
-                Log.i("Order", currentOrder.toString());
+                if(listSize > 0)
+                {
+                    listItems.remove(listSize - 1);
+                    adapter.notifyDataSetChanged();
+                    currentOrder.getCurrentOrder().remove(listSize - 1);
+                }
+
+                for(String s: listItems)
+                    Log.i("listItems", "" + s);
+
+
+                Log.i("Oder", "" + currentOrder.toString());
+
             }
         });
     }
